94c2e6357b95b19db28c29707d0537a013ef7ea4
Bob Effinger <Regniffe3@gmail.com>
2018-03-14 09:10:07 -0500

    added rectangle functions

--- GrabSnap.m
+++ GrabSnap.m
@@ -2,3 +2,8 @@
 filename = '.\Test_Images\police_dog_training_PubD.jpg';
 img = imread(filename);
 imshow(img);
+
+rect=getrect                           % select rectangle
+
+I2=imcrop(img,rect);
+imshow(I2)
