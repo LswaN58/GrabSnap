fda796cb22bf2e3be6018c29bdfd33490e86c51b
Bob Effinger <Regniffe3@gmail.com>
2018-03-20 16:35:15 -0500

    ConvertImDataToGMMData was implemented

--- ConvertImDataToGMMData.m
+++ ConvertImDataToGMMData.m
@@ -1,5 +1,6 @@
 function [ GMM_data ] = ConvertImDataToGMMData( img )
-    warning('STUB! ConvertImDataToGMMData only returns the original image.')
-    GMM_data = img;
+    [sizeY, sizeX, ~] = size(img);
+    [X,Y] = meshgrid(1:sizeX,1:sizeY);
+    GMM_data = cat(img, X, Y);
 end
 
