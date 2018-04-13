b37b37583d3067702e8ffdfd5c508d3bed6518e1
Bob Effinger <Regniffe3@gmail.com>
2018-04-13 18:46:38 -0500

    normalized x and y values in comvertImDataToGMMData.m

--- ConvertImDataToGMMData.m
+++ ConvertImDataToGMMData.m
@@ -1,6 +1,6 @@
 function [ GMM_data ] = ConvertImDataToGMMData( img )
     [sizeY, sizeX, ~] = size(img);
     [X,Y] = meshgrid(1:sizeX,1:sizeY);
-    GMM_data = cat(3, img, X, Y);
+    GMM_data = cat(3, img, (X/sizeX * 255), (Y/sizeY * 255));
 end
 
