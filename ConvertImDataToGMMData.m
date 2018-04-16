7f7b4b4d55309d03ed0ebf240629df9613953de8
Luke Swanson <swansonl@sonic.cs.wisc.edu>
2018-04-16 15:31:34 -0500

    Changed code to use 5 dimensions again, with normalization stuff from Bob.

--- ConvertImDataToGMMData.m
+++ ConvertImDataToGMMData.m
@@ -1,7 +1,7 @@
 function [ GMM_data ] = ConvertImDataToGMMData( img )
-    %[sizeY, sizeX, ~] = size(img);
-    %[X,Y] = meshgrid(1:sizeX,1:sizeY);
-    %GMM_data = cat(3, img, X, Y);
-    GMM_data = img;
+    [sizeY, sizeX, ~] = size(img);
+    [X,Y] = meshgrid(1:sizeX,1:sizeY);
+    GMM_data = cat(3, img, (X/sizeX * 255), (Y/sizeY * 255));
+    %GMM_data = img;
 end
 
